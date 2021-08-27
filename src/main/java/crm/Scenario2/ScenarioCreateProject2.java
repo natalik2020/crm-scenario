324683044f5ac08a0140f0561f44b0e9fd912849
geekbrains <natalik_1991@mail.ru>
2021-08-27 14:09:28 +0300

    lesson 3

--- src/main/java/crm/Scenario2/ScenarioCreateProject2.java
+++ src/main/java/crm/Scenario2/ScenarioCreateProject2.java
@@ -10,6 +10,7 @@ import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.util.List;
+import java.util.UUID;
 
 public class ScenarioCreateProject2 {
     private static WebDriver driver;
@@ -17,15 +18,50 @@ public class ScenarioCreateProject2 {
     public static void main(String[] args) throws InterruptedException {
         WebDriverManager.chromedriver().setup();
 
-        //scenarioWithExtention();
-
         driver = new ChromeDriver();
-        //runJsScriptExample();
 
         driver.manage().window().maximize();
         WebDriverWait webDriverWait = new WebDriverWait(driver, 5);
 
         loginToCrm();
+
+        Actions actions = new Actions(driver);
+        WebElement projectMenuElement = driver.findElement(By.xpath("//a/span[text()='Справочники']"));
+        actions.moveToElement(projectMenuElement).perform();
+
+        driver.findElement(By.xpath("//li[@data-route='crm_ownershiptype_index']/a")).click();
+
+        {
+            String xpathExpression = "//a[text()='Новая']";
+            webDriverWait.until(ExpectedConditions.elementToBeClickable(By.xpath(xpathExpression)));
+            driver.findElement(By.xpath(xpathExpression)).click();
+        }
+
+        // Наимен
+        {
+            Thread.sleep(2000);
+            String xpath = "//input[contains(@id, 'crm_ownershiptype_name')]";
+            webDriverWait.until(ExpectedConditions.elementToBeClickable(By.xpath(xpath)));
+
+            WebElement element = driver.findElement(By.xpath(xpath));
+            element.sendKeys("Наименование" + UUID.randomUUID());
+            element.sendKeys(Keys.ENTER);
+        }
+
+        //сохранить
+        {
+            String xpathExpression = "//button[contains(@type, 'submit')and contains(@class, 'btn-primary')]";
+
+            Actions actions1 = new Actions(driver);
+            WebElement projectMenuElement1 = driver.findElement(By.xpath(xpathExpression));
+            actions1.moveToElement(projectMenuElement1).perform();
+
+            webDriverWait.until(ExpectedConditions.elementToBeClickable(By.xpath(xpathExpression)));
+            driver.findElement(By.xpath(xpathExpression)).click();
+        }
+
+        Thread.sleep(5000);
+        driver.switchTo().parentFrame();
     }
 
     public static void loginToCrm() {
